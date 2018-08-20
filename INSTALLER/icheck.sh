5cfcf8a1fa4907bebd167abb9718dca179ad1005
Ultima mirror <41716542+ultimamirror@users.noreply.github.com>
2018-08-20 00:22:17 -0400

    Update icheck.sh
    Changed cron task from 20 minutes to 10 minutes

--- INSTALLER/icheck.sh
+++ INSTALLER/icheck.sh
@@ -9,6 +9,6 @@ chmod +x mncheck.sh
 #setup crontab
 
 crontab -l > mycron
-echo "*/20 * * * * $HOME/mncheck.sh > /dev/null 2>&1" >> mycron
+echo "*/10 * * * * $HOME/mncheck.sh > /dev/null 2>&1" >> mycron
 crontab mycron
 rm mycron
